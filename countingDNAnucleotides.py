8c700297ec1321c71c2b25b874345c902bb36935
PhilipHoltzman <phil.holtzman@gmail.com>
2016-03-15 19:39:43 -0600

    adding first exercise

---
+++ countingDNAnucleotides.py
@@ -0,0 +1,29 @@
+#! Python3
+
+# string analyzer to sort DNA Nucleotides
+
+
+
+dataset = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+
+
+adenine = ''
+cytosine =''
+guanine = ''
+thymine = ''
+
+output = ''
+
+
+def adenineFunction():
+	print ('\ncalculating adenine...')
+
+def cytosineFunction():
+	print ('\ncalculating cytosine...')
+
+def guanineFunction():
+	print ('\ncalculating guanine...')
+	
+
+def thymineFunction():
+	print ('\ncalculating thymine...')
