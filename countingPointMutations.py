3fc2d1a169f87e8f50982d4e4b1543d893eb4757
PhilipHoltzman <phil.holtzman@gmail.com>
2016-03-19 18:18:53 -0600

    finished counting point mutations exercise

--- countingPointMutations.py
+++ countingPointMutations.py
@@ -4,4 +4,33 @@
 
 
 
+# initial data
+s = ''
+t = ''
 
+# splitting the data into individual letters
+s = list(s)
+t = list(t)
+
+# Hamming Distance Var
+hDistance = 0
+# counting var
+n = 0
+
+# matching algorithm 
+for i in s:
+	if i == t[n]:
+		print('match')
+		n = n + 1
+
+	else:
+		print('no match')
+		n = n + 1
+		hDistance = hDistance + 1
+
+# print solution
+print(hDistance)
+
+	
+
+	
